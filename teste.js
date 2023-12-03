d351e57cf1525b1e4d2ce985b06c4c36caeb4768
RILDO <rildosouza88@hotmail.com>
2023-12-03 00:39:22 -0300

    Add files via upload

--- teste.js
+++ teste.js
@@ -2,9 +2,9 @@
 somar(10)//<==digite o número aqui
 function somar (value) { 
 
-    let mult3 = 0
-    let mult5 = 0
-    let mult = mult3 || mult5
+    let mult3 = 0 //representa os números divisiveis por 3
+    let mult5 = 0 //representa os números divisiveis por 5
+    let mult = mult3 || mult5 //variável hibrida, recebe o valor das variáveis
    
     for(i = 0; i < value; i++) { //faz a varredura do zero até o número inserido, verifica se é divisivél por 3 ou 5 e retorna para o loop, caso contrário executa o console
         if (i % 3 === 0 || i % 5 === 0) {
@@ -12,7 +12,7 @@ function somar (value) {
               console.log(i)//mostra os números somados         
         } 
     }
-    console.log('O resultado é: ' + mult) //mostra o resultado
+    console.log('O resultado é: ' + mult) //mostra o resultado da soma
     } 
     
 
